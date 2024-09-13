5f96d47e3b87e26d32c9e2c75e146c0fd71ab5b9
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-09-13 16:17:03 +0200

    cors

--- src/app.ts
+++ src/app.ts
@@ -17,12 +17,14 @@ const app = express();
 
 // CORS configuration
 app.use(
-  cors({
-    origin: process.env.CORS_ALLOW as string || "https://www.futurefocus.co.rw",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
+  cors(
+  //   {
+  //   origin: process.env.CORS_ALLOW as string || "https://www.futurefocus.co.rw",
+  //   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  //   allowedHeaders: ["Content-Type", "Authorization"],
+  //   credentials: true,
+  // }
+)
 );
 
 app.use(express.json());
