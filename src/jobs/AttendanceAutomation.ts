6642d16a83013d0fe1cd2390e7be821d2d74e2d1
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-09-22 19:56:21 +0200

    cron

--- src/jobs/AttendanceAutomation.ts
+++ src/jobs/AttendanceAutomation.ts
@@ -40,20 +40,4 @@ export const dropout = () => {
   });
 };
 
-// export const dailyAttendance =async (req:Request,res:Response) => {
-//     try {
-//       const students = await Student.find({ status: "started" });
-//       for (const student of students) {
-//         await Attendance.create({
-//           studentId: student._id,
-//         });
-//       }
-//       res.status(200).json({message:"attendance"})
-//       console.log("Attendance created");
-//     } catch (error) {
-//       res.status(500).json({ message: error });
-
-//       console.error("Error in dailyAttendance:", error);
-//     }
-// };
 
