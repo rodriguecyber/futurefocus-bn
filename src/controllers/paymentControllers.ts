722d1a851c610277fc41988ea1f3a1e6c3063e37
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-10-03 10:39:36 +0200

    payment

--- src/controllers/paymentControllers.ts
+++ src/controllers/paymentControllers.ts
@@ -9,10 +9,7 @@ import Student from "../models/Students";
 export class PaymentController {
   static SchoollFees = async (req: Request, res: Response) => {
     const { id } = req.params;
-     const token = req.headers.authorization?.split(" ")[1];
-     const userId  = decodeToken(token as string)
-     const user = Admin.find 
-    const { amount,method } = req.body;
+    const { amount,method,user } = req.body;
     try {
       const payment = await Payment.findOneAndUpdate(
         { studentId: id },
