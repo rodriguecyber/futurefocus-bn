1575fc20f9fd6fdd86e571d5020871309a1e3b15
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-10-09 11:45:00 +0200

    yt videos

--- src/models/youtube.ts
+++ src/models/youtube.ts
@@ -1,8 +1,8 @@
 import { model, Schema } from "mongoose";
-import { CourseTypes } from "../types/Types";
 
 const VideoSchema = new Schema({
   url: { type: String, required: true },
+  type: { type: String, required: true,enum:['video','audio'] },
 });
 const Video = model("YoutubeVideo", VideoSchema);
 export default Video;
