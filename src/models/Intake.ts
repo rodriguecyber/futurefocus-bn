a666c23fc284fdf1e9fee4eda50bafa622d2a5d5
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-11-16 16:08:10 +0200

    /ff

--- src/models/Intake.ts
+++ src/models/Intake.ts
@@ -1,15 +1,33 @@
-import mongoose, { model, Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 
-   
-interface IIntake{
-    intake:string
+interface IIntake {
+  intake: string;
 }
-   const IntakeSchema = new Schema<IIntake>({
-     intake:{type:String,required:true},
-   },
-   {
-    timestamps:true
-   }
-)
-   const Intake = model<IIntake>("intake",IntakeSchema)
-   export default Intake
+const IntakeSchema = new Schema<IIntake>(
+  {
+    intake: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Intake = model<IIntake>("intake", IntakeSchema);
+export default Intake;
+
+// shift model
+interface IShift {
+  shift: string;
+  start: Date;
+  end: Date;
+}
+const ShiftSchema = new Schema<IShift>(
+  {
+    shift: { type: String, required: true },
+    start: { type: Date, required: true },
+    end: { type: Date, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+export const Shift = model<IShift>("shift", ShiftSchema);
