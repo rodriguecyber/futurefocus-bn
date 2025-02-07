1799677cf3bbd2c982c893c0fc861b685f3033b1
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2025-02-07 11:45:06 +0200

    corse

--- src/routes/StudentsRoutes.ts
+++ src/routes/StudentsRoutes.ts
@@ -3,25 +3,25 @@ import { StudentControllers } from "../controllers/StudentsController";
 import { getAttendance,
      updateAttendance 
     } from "../controllers/Attendance";
-import { isloggedIn } from "../middleware/isLoggedIn";
 import { isVerified } from "../middleware/isVerified";
+import { isloggedIn } from "../middleware/isLoggedIn";
 
 
 export const StudentRoutes =  Router()
 StudentRoutes.post('/apply',StudentControllers.apply)
-StudentRoutes.post('/past',StudentControllers.pastRecord)
+StudentRoutes.post('/past',isloggedIn,StudentControllers.pastRecord)
 StudentRoutes.post('/techup',StudentControllers.techupapply)
-StudentRoutes.post('/techup/notify',StudentControllers.notifyTechups)
+StudentRoutes.post('/techup/notify',isloggedIn, StudentControllers.notifyTechups)
 StudentRoutes.get('/techup',StudentControllers.techUpStudent)
 StudentRoutes.get("/",isloggedIn, isVerified,
     // (req,res,next)=>hasAcces(req,res,next,'student'),
     StudentControllers.students)
-StudentRoutes.delete('/:id',StudentControllers.delete)
-StudentRoutes.put('/:id',StudentControllers.changeStatus)
-StudentRoutes.post('/register',StudentControllers.registerNew)
-StudentRoutes.put("/attend/:studentId", updateAttendance);
-StudentRoutes.get("/attendance", getAttendance);
-StudentRoutes.put("/comment/:id", StudentControllers.AddComment);
-StudentRoutes.put("/update/:id", StudentControllers.Update);
+StudentRoutes.delete('/:id',isloggedIn,StudentControllers.delete)
+StudentRoutes.put('/:id',isloggedIn, StudentControllers.changeStatus)
+StudentRoutes.post('/register', isloggedIn,StudentControllers.registerNew)
+StudentRoutes.put("/attend/:studentId",isloggedIn, updateAttendance);
+StudentRoutes.get("/attendance", isloggedIn,getAttendance);
+StudentRoutes.put("/comment/:id",isloggedIn, StudentControllers.AddComment);
+StudentRoutes.put("/update/:id",isloggedIn, StudentControllers.Update);
 
 
