e10fae6cbcb1e9ca6fee96b714f3b7433e27c8be
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2025-02-07 10:24:01 +0200

    cashflow

--- src/routes/cashFlow.ts
+++ src/routes/cashFlow.ts
@@ -3,6 +3,8 @@ import { cashflowControler } from "../controllers/cashFlowControlers";
 import { hasAcces } from "../middleware/hasAcces";
 
 export const cashRouter = Router()
-cashRouter.get("/",(req,res,next)=>hasAcces(req,res,next,'cashflow'), cashflowControler.getAll)
+cashRouter.get("/",
+    // (req,res,next)=>hasAcces(req,res,next,'cashflow'),
+     cashflowControler.getAll)
 cashRouter.post("/", cashflowControler.newData)
 cashRouter.delete("/:id", cashflowControler.delete)
